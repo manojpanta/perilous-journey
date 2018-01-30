1491854e2e41192d0d4780b7752e3ee4aace5d04
Manoj Panta <manojpanta95@gmail.com>
2018-01-30 16:43:37 -0700

    test added

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,11 +2,45 @@ require "./lib/node"
 
 class LinkedList
 
-attr_reader :head
+attr_reader :head, :count
+
+
 
   def initialize
-    @head =  nil
+    @head = nil
+    @count = 0
   end
+  # # def head
+  # #
+  # # end
+  # #
+  def append(surname)
+    if @head.nil?
+      @head = Node.new(surname)
+      @count += 1
+    else
+    end
+  end
+
+
+
+  def to_string
+     "The #{@head.surname} Family"
+
+
+  end
+
+  #   def to_string
+  #
+  # end
+  #
+  # # def next_node
+  #   LinkedList.new = Node.new
+  # #   # LinkedList.new(head)
+  # end
+
+
+
 
 
 end
