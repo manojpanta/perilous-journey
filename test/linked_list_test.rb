462f2e2bc27454d5f7f9d3b21ccec57eb9d8c050
Manoj Panta <manojpanta95@gmail.com>
2018-01-30 16:44:38 -0700

    new test added agin

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -13,16 +13,56 @@ class LinkedListTest < Minitest::Test
   def test_head
     list = LinkedList.new
     assert_equal nil, list.head
+    # require 'pry'; binding.pry
   end
+
   def test_list_append
     list = LinkedList.new
-    assert_instance_of Node, list.append("West")
+
+    list.append("West")
+
+    assert_equal "West", list.head.surname
+    assert_nil list.head.next_node
+  end
+  #
+  def test_list_count
+    list = LinkedList.new
+    list.append ("West")
+    assert_equal 1, list.count
+  end
+
+  def test_list_to_string
+    list = LinkedList.new
+    list.append ("West")
+
+    assert_equal "The West Family", list.to_string
+    # require 'pry';binding.pry
   end
 
 
+  # def test_head_next_node
+  #   list = LinkedList.new
+  #   assert_equal nil, list.head.next_node
+  # end
 
-
-
+  # def test_name_list
+  #   list = LinkedList.new
+  #   assert_instance_of LinkedList, list
+  # end
+  # #
+  # def test_head_next_node
+  #   list = LinkedList.new
+  #   assert_equal nil, list.next_node
+  # end
+  # # def test_next_node
+  #   list = LinkedList.new
+  #   assert_equal nil, list.head.next_node
+  # end
+  # #
+  # def test_head_next_node
+  #   list = LinkedList.new
+  #   assert_equal nil, list.head.next_node
+  # end
   # def test_surname
   #   list = LinkedList.new
   #   assert_equal @surname = "West", list.append
